b88b58ce5ee2918565104342987ac19665bbcb00
angelddeleon <angeldeleonpr00@gmail.com>
2024-12-15 20:42:04 -0400

    Adding map in the project section and adding images of the project

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,13 +1,33 @@
 import './Project.css'
 import ProjectImage from '../../assets/projectImage.png'
+
+import img1 from '../../assets/imageProjects/autowork1.png';
+import img2 from '../../assets/imageProjects/autowork2.png';
+import img3 from '../../assets/imageProjects/autowork3.png';
+import img4 from '../../assets/imageProjects/autowork4.png';
+
+
+
 import { useLanguage } from '../../hooks/LanguageContext';
 
+import { ProjectCard } from './ProjectCard/ProjectCard';
+
 export function ProjectSection() {
-    const { language, toggleLanguage } = useLanguage();
+    const { language } = useLanguage();
 
     const indexLanguage = language === 'en' ? 0 : 1 
 
-    let textLanguage = [ {h1 : 'Projects' } , {h1 : 'Proyectos' }]
+    let textLanguage = [{h1 : 'Projects' } , {h1 : 'Proyectos' }]
+
+    const projects = [
+        {
+            id:1,
+            images: [img1, img2, img3, img4],
+            nameProject: "AutoWork",
+            description: "Hola"
+        }
+    ]
+    
 
 
     return(
@@ -15,6 +35,17 @@ export function ProjectSection() {
             <h2 id='project'>{textLanguage[indexLanguage].h1}</h2>
 
             <div className='project-section-cards'>
+
+                {
+
+                    projects.map((project) =>{
+                        return(
+                            <ProjectCard key={project.id} images={project.images} titleProject={project.nameProject} description={project.description} />
+                        )
+                        
+                    })
+                    
+                }
 
                 <div className="project-card">
                     <img src={ProjectImage} alt="" />
@@ -27,7 +58,7 @@ export function ProjectSection() {
                         <a className='card-button view' href="#">View</a>
                         <a className='card-button code' href="#">Code</a>
                     </div>
-                 </div>
+                </div>
 
                  <div className="project-card">
                     <img src={ProjectImage} alt="" />
---
+++ src/Components/Projects/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+
+import './ProjectCard.css'
+
+
+export function ProjectCard ({images, titleProject, description}) {
+
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    // Cambiar la imagen cada segundo
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 2000);
+
+        return () => clearInterval(interval); // Limpiar el intervalo al desmontar
+    }, [images.length]);
+
+    // Función para abrir el modal
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    // Función para cerrar el modal
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+
+    return (
+        <div className="project-card">
+            <img src={images[currentImageIndex]} alt={titleProject} onClick={openModal} />
+            <div className="project-card-text">
+                <h3>{titleProject}</h3>
+                <p>{description}</p>
+            </div>
+            <div className="project-card-buttons">
+                <a className='card-button view' href="#">View</a>
+                <a className='card-button code' href="#">Code</a>
+            </div>
+
+            {/* Modal para mostrar la imagen completa */}
+            {isModalOpen && (
+                <div className="modal" onClick={closeModal}>
+                    <img src={images[currentImageIndex]} alt={titleProject} className="modal-image" />
+                </div>
+            )}
+        </div>
+    );
+
+}
